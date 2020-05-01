76d6cd7a78430cad350db54681299b2f334b1749
Vision Lsm <vision.lsm.2012@gmail.com>
2020-05-02 02:51:42 +0800

    Added support sr3;

---
+++ boards/stm32-sr3.h
@@ -0,0 +1,30 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <stdbool.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+/*
+ * Board definitions for "redpill" and "bluepill" boards
+ * These are low cost boards that are quite wide spread, and might come in many brands.
+ * Wiki:
+ *      http://wiki.stm32duino.com/index.php?title=Blue_Pill
+ *      http://wiki.stm32duino.com/index.php?title=Red_Pill
+ */
+
+#define BOARD_USE_DEBUG_PINS_AS_GPIO false /* We don't have to disable SWD to access GPIO's on these boards */
+
+#define BOARD_RCC_LED                RCC_GPIOB
+#define BOARD_PORT_LED               GPIOB
+#define BOARD_PIN_LED                GPIO0
+#define BOARD_LED_HIGH_IS_BUSY       true
+
+#define BOARD_RCC_USB_PULLUP         RCC_GPIOA
+#define BOARD_PORT_USB_PULLUP        GPIOA
+#define BOARD_PIN_USB_PULLUP         GPIO12
+#define BOARD_USB_HIGH_IS_PULLUP     true
+
+/* Currently you can only use SPI1, since it has highest clock. */
+
+#endif /* __BOARD_H__ */
