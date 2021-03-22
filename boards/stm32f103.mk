b661e9ea0699185e00cfd836d871b583565dcf14
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-03-22 04:55:28 +0200

    Fix linking issues

--- boards/stm32f103.mk
+++ boards/stm32f103.mk
@@ -1,4 +1,4 @@
 ARCH_FLAGS = -DSTM32F1 -mthumb -mcpu=cortex-m3 -msoft-float -mfix-cortex-m3-ldrd
-LDSCRIPT   = libopencm3/lib/stm32/f1/stm32f103x8.ld
+LDSCRIPT   = boards/ld/stm32f100x8.ld
 LIBOPENCM3 = libopencm3/lib/libopencm3_stm32f1.a
 OPENCM3_MK = lib/stm32/f1
